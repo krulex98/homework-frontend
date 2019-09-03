6c776dd4bcd7061e767e89640287c1a9d07b62dd
Alexandra Kruchinina <krulex98@gmail.com>
2019-09-03 11:59:33 +0300

    added jsDoc for functions

--- source/plainify.js
+++ source/plainify.js
@@ -1,9 +1,29 @@
 'use strict';
 
+/**
+  * @description check if object is empty
+  * 
+  * @param {object} obj
+  * @returns {boolean}
+  */
 const isEmpty = (obj) =>  Object.keys(obj).length === 0;
 
+/**
+  * @description check if object is object
+  * 
+  * @param {object} obj
+  * @returns {boolean}
+  */
 const isObject = (obj) => typeof obj === 'object';
 
+/**
+  * @description function turns an object with several levels of nesting
+  * into an object with one level of nesting, 
+  * where the property is the path
+  * 
+  * @param {object} obj
+  * @returns {object}
+  */
 const innerPlainify = (object) => {	
 	let result = {};
 	for (const prop in object) {
@@ -19,11 +39,18 @@ const innerPlainify = (object) => {
 	return result;
 };
 
-const plainify = (object) => {
-	if(!isObject(object) || object == null ){
-		return object;
+/**
+  * @description if the data is nonnullable and is object 
+  * then the function plainify is applied, else return data
+  * 
+  * @param {*} object
+  * @returns {*}
+  */
+const plainify = (data) => {
+	if(!isObject(data) || data == null ){
+		return data;
 	}
 	
-	return innerPlainify(object);
+	return innerPlainify(data);
 	
 };
